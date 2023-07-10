9533632a9e22589f06585ee7f5f68b581788d1a0
Ben Blair <benchrisblair@users.noreply.github.com>
2023-07-10 11:51:23 -0700

    Update formatting

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,13 +8,13 @@ import datetime
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 django_default_files = [
-    "{{cookiecutter.project_slug}}/__init__.py",
-    "{{cookiecutter.project_slug}}/asgi.py",
-    "{{cookiecutter.project_slug}}/settings.py",
-    "{{cookiecutter.project_slug}}/urls.py",
-    "{{cookiecutter.project_slug}}/wsgi.py",
+    "{{ cookiecutter.project_slug }}/__init__.py",
+    "{{ cookiecutter.project_slug }}/asgi.py",
+    "{{ cookiecutter.project_slug }}/settings.py",
+    "{{ cookiecutter.project_slug }}/urls.py",
+    "{{ cookiecutter.project_slug }}/wsgi.py",
     "manage.py",
-    "templates"
+    "templates",
 ]
 
 
@@ -23,7 +23,7 @@ def generate_django_secret():
     from django.core.management.utils import get_random_secret_key
     django_secret = get_random_secret_key()
 
-    with open('{{cookiecutter.project_slug}}/settings.py', "r") as file:
+    with open('{{ cookiecutter.project_slug }}/settings.py', "r") as file:
         content = file.read()
 
     # Construct the pattern to match the variable assignment
@@ -32,7 +32,7 @@ def generate_django_secret():
     # Replace the variable assignment with the new value
     replaced_content = re.sub(pattern, "{} = '{}'".format('SECRET_KEY', django_secret), content)
 
-    with open('{{cookiecutter.project_slug}}/settings.py', "w") as file:
+    with open('{{ cookiecutter.project_slug }}/settings.py', "w") as file:
         file.write(replaced_content)
 
 def remove_file(filepath):
@@ -62,28 +62,24 @@ def get_py_ver():
 
 if __name__ == '__main__':
 
-    if '{{ cookiecutter.create_author_file }}' != 'y':
-        remove_file('docs/AUTHORS.md')
-
-    if 'Not open-source' == '{{ cookiecutter.open_source_license }}':
+    if '{{ cookiecutter.open_source_license }}' == 'None':
         os.remove('LICENSE')
 
     filepath = "docs/sphinx/conf.py"
     var_name = "copyright"
-    new_val = f'{datetime.datetime.now().year}, {{cookiecutter.copyright_entity}}'
+    new_val = f'{datetime.datetime.now().year}, {{ cookiecutter.copyright_entity }}'
     replace_var_val(filepath, var_name, new_val)
 
-    if '{{cookiecutter.generate_requirements_files}}'.lower() == 'y':
+    if '{{ cookiecutter.generate_requirements_files }}'.lower() == 'y':
         subprocess.run(['pip', 'install', 'pip-tools'])
         subprocess.run(['pip-compile', '-o', 'requirements.txt', 'pyproject.toml', '--resolver=backtracking'])
         subprocess.run(['pip-compile', '-o', 'dev_requirements.txt', 'pyproject.toml', '--resolver=backtracking', '--all-extras'])
         subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
         subprocess.run(['pip', 'install', '-r', 'dev_requirements.txt'])
 
-    if '{{cookiecutter.create_django_default_files}}'.lower() == 'y':
-        if '{{cookiecutter.generate_django_secret_key}}'.lower() == 'y':
+    if '{{ cookiecutter.create_django_default_files }}'.lower() == 'y':
+        if '{{ cookiecutter.generate_django_secret_key }}'.lower() == 'y':
             generate_django_secret()
-
     else:
         for filepath in django_default_files:
             remove_file(filepath)
