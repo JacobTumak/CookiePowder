99bf10e8cd74a78b716ac36dc98286d8aa6ff613
Ben Blair <benchrisblair@users.noreply.github.com>
2023-07-13 10:09:02 -0700

    Remove fn to add copyright to sphinx config

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,6 @@ import os
 import re
 import sys
 import subprocess
-import datetime
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -15,6 +14,16 @@ django_default_files = [
     "{{ cookiecutter.project_slug }}/wsgi.py",
     "manage.py",
 ]
+
+
+def get_py_ver():
+    version_info = sys.version_info
+    version_string = f"{version_info.major}.{version_info.minor}.{version_info.micro}"
+    return version_string
+
+
+def remove_file(filepath):
+    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
 def generate_django_secret_key():
@@ -35,40 +44,10 @@ def generate_django_secret_key():
         file.write(replaced_content)
 
 
-def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
-
-
-def replace_var_val(file_path, var_name, new_var_val):
-
-    with open(file_path, "r") as file:
-        content = file.read()
-
-    # Construct the pattern to match the variable assignment
-    pattern = r"{} = .*".format(re.escape(var_name))
-
-    # Replace the variable assignment with the new value
-    replaced_content = re.sub(pattern, "{} = '{}'".format(var_name, new_var_val), content)
-
-    with open(file_path, "w") as file:
-        file.write(replaced_content)
-
-
-def get_py_ver():
-    version_info = sys.version_info
-    version_string = f"{version_info.major}.{version_info.minor}.{version_info.micro}"
-    return version_string
-
-
 if __name__ == '__main__':
 
     if '{{ cookiecutter.open_source_license }}' == 'None':
         os.remove('LICENSE')
-
-    filepath = "docs/source/conf.py"
-    var_name = "copyright"
-    new_val = f'{datetime.datetime.now().year}, {{ cookiecutter.copyright_entity }}'
-    replace_var_val(filepath, var_name, new_val)
 
     if '{{ cookiecutter.generate_requirements_files }}'.lower() == 'y':
         subprocess.run(['pip', 'install', 'pip-tools'])
